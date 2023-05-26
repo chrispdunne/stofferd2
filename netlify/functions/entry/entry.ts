f57fe7c15ed921186d3b531fd385e04f2b80bab8
Chris Dunne <chris.dunne@ihsmarkit.com>
2023-05-26 13:01:01 +0100

    ignore password cases

--- netlify/functions/entry/entry.ts
+++ netlify/functions/entry/entry.ts
@@ -1,9 +1,10 @@
 import { Handler } from '@netlify/functions';
 
 export const handler: Handler = async event => {
-	const pwords = process.env.SITE_PASSWORDS;
+	const pwords = process.env.SITE_PASSWORDS?.toLowerCase();
 	const body = JSON.parse(event.body ?? '');
-	const match = pwords && pwords.split(' ').includes(body.typed);
+	const match =
+		pwords && pwords.split(' ').includes(body.typed.toLowerCase());
 
 	return {
 		statusCode: 200,
