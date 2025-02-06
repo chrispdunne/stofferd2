55b25d1b1936d8fc14aaa214c4c981ae9dc1b753
Chris Dunne <chris.dunne@ihsmarkit.com>
2025-02-06 14:52:44 +0000

    eye section tweak

--- src/Components/Intro/index.tsx
+++ src/Components/Intro/index.tsx
@@ -1,18 +1,23 @@
 import { Link } from 'react-router-dom';
 import Eye from '../Eye';
-import { IntroContainer, IntroSubtitle, IntroTitle } from './styles';
+import {
+	IntroContainer,
+	IntroTextContainer,
+	IntroSubtitle,
+	IntroTitle
+} from './styles';
 
 export default function Intro() {
 	return (
-		<>
-			<IntroContainer>
+		<IntroContainer>
+			<IntroTextContainer>
 				<IntroSubtitle>React, TypeScript, Node, C#</IntroSubtitle>
 				<IntroTitle>Full stack developer</IntroTitle>
 				<Link className="btn" to="/about">
 					Find out more
 				</Link>
-			</IntroContainer>
+			</IntroTextContainer>
 			<Eye />
-		</>
+		</IntroContainer>
 	);
 }
--- src/Components/Intro/styles.ts
+++ src/Components/Intro/styles.ts
@@ -2,18 +2,21 @@ import styled from 'styled-components';
 import { WorkSubtitle, WorkTitle } from '../../Pages/styles';
 
 export const IntroContainer = styled.div`
+	min-height: 100vh;
+`;
+
+export const IntroTextContainer = styled.div`
 	position: absolute;
 	top: 60%;
 	z-index: 2;
 	text-align: center;
 	color: #fff;
 	width: 100%;
-	margin-top: 50px;
+
 	/* 980px */
 	@media only screen and (min-width: 61.25em) {
 		left: 0;
 		top: 50%;
-		margin-top: 0px;
 		transform: translateY(-50%);
 		text-align: left;
 		margin-left: 50px;
