743c5fa90d12615111cd514745403a679ca33ed1
Chris Dunne <chris.dunne@ihsmarkit.com>
2025-09-18 08:56:36 +0100

    seo tweak

--- src/Components/Logo/Logo2d.tsx
+++ src/Components/Logo/Logo2d.tsx
@@ -1,18 +1,36 @@
+import { useLocation } from 'react-router-dom';
 import { logoPixels } from '../../const/logo';
 import { Grid } from './styles';
 
-interface Props {
+interface Logo2dProps {
 	background?: string;
 }
+interface HeaderElementProps {
+	isHomePage?: boolean;
+	children: React.ReactNode;
+	style?: React.CSSProperties;
+}
 
-export default function Logo2d({ background = '#fff' }: Props) {
+function HeaderElement({ isHomePage, children, style }: HeaderElementProps) {
+	return isHomePage ? (
+		<h1 style={style}>{children}</h1>
+	) : (
+		<h2 style={style}>{children}</h2>
+	);
+}
+
+export default function Logo2d({ background = '#fff' }: Logo2dProps) {
 	const style = { background };
-
+	const { pathname } = useLocation();
+	const isHomePage = pathname === '/';
 	return (
-		<Grid className="logo-grid">
-			{logoPixels.map((pixel: string, i: number) => (
-				<div key={i} className={pixel} style={style} />
-			))}
-		</Grid>
+		<HeaderElement isHomePage={isHomePage} style={{ fontSize: 'inherit' }}>
+			<span className="sr-only">Chris Dunne</span>
+			<Grid className="logo-grid">
+				{logoPixels.map((pixel: string, i: number) => (
+					<div key={i} className={pixel} style={style} />
+				))}
+			</Grid>
+		</HeaderElement>
 	);
 }
